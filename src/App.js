90388207d49988a9fc8d4ddc9a7b9234594266dc
Brian1238765567 <qwerty445245245245@gmail.com>
2022-04-24 23:01:56 -0400

    appback

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { 
+  AddMovie 
+} from './ui-components';
+
+import { 
+  MovieBar 
+} from './ui-components';
+
+import { 
+  MovieCompCollection 
+} from './ui-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <MovieBar />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <MovieCompCollection />
+
+        <AddMovie />
+
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+   
       </header>
     </div>
   );
