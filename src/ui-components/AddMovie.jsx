22b3e104a635e072d9d34c9aee285cc0b710981a
Brian1238765567 <qwerty445245245245@gmail.com>
2022-04-24 23:06:42 -0400

    addmovie2

---
+++ src/ui-components/AddMovie.jsx
@@ -0,0 +1,224 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import {
+  getOverrideProps,
+  useDataStoreCreateAction,
+  useStateMutationAction,
+} from "@aws-amplify/ui-react/internal";
+import { Movie } from "../models";
+import {
+  Button,
+  Divider,
+  Flex,
+  Icon,
+  Text,
+  TextField,
+  View,
+} from "@aws-amplify/ui-react";
+export default function AddMovie(props) {
+  const { movie, overrides, ...rest } = props;
+  const [
+    textFieldThreeZeroSevenFiveTwoEightEightZeroValue,
+    setTextFieldThreeZeroSevenFiveTwoEightEightZeroValue,
+  ] = useStateMutationAction("");
+  const [
+    textFieldThreeZeroSevenFiveTwoEightEightOneValue,
+    setTextFieldThreeZeroSevenFiveTwoEightEightOneValue,
+  ] = useStateMutationAction("");
+  const buttonOnClick = useDataStoreCreateAction({
+    fields: {
+      Name: textFieldThreeZeroSevenFiveTwoEightEightZeroValue,
+      image_url: textFieldThreeZeroSevenFiveTwoEightEightOneValue,
+      RTscore: "95",
+      MCscore: "67",
+      IMDbscore: "7.6",
+      Wikiurl: "google.com",
+    },
+    model: Movie,
+  });
+  return (
+    <Flex
+      gap="16px"
+      direction="column"
+      width="640px"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "AddMovie")}
+    >
+      <Flex
+        gap="24px"
+        direction="column"
+        shrink="0"
+        alignSelf="stretch"
+        objectFit="cover"
+        position="relative"
+        padding="24px 24px 24px 24px"
+        {...getOverrideProps(overrides, "Content")}
+      >
+        <Flex
+          gap="16px"
+          direction="row"
+          alignItems="center"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Edit Profile")}
+        >
+          <View
+            width="24px"
+            height="24px"
+            shrink="0"
+            overflow="hidden"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Icon")}
+          >
+            <Icon
+              width="14px"
+              height="14px"
+              viewBox={{ minX: 0, minY: 0, width: 14, height: 14 }}
+              paths={[
+                {
+                  d: "M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z",
+                  fill: "rgba(13,26,38,1)",
+                  fillRule: "nonzero",
+                },
+              ]}
+              position="absolute"
+              top="20.83%"
+              bottom="20.83%"
+              left="20.83%"
+              right="20.83%"
+              {...getOverrideProps(overrides, "Vector")}
+            ></Icon>
+          </View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(13,26,38,1)"
+            lineHeight="20px"
+            textAlign="left"
+            display="flex"
+            direction="column"
+            justifyContent="flex-start"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="Add Movie"
+            {...getOverrideProps(overrides, "Add Movie")}
+          ></Text>
+        </Flex>
+        <Divider
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Divider30752875")}
+        ></Divider>
+        <Flex
+          gap="16px"
+          direction="column"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Forms")}
+        >
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Name"
+            placeholder="Drive"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            value={textFieldThreeZeroSevenFiveTwoEightEightZeroValue}
+            onChange={(event) => {
+              setTextFieldThreeZeroSevenFiveTwoEightEightZeroValue(
+                event.target.value
+              );
+            }}
+            {...getOverrideProps(overrides, "TextField30752880")}
+          ></TextField>
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Image URL"
+            placeholder="www.google.com"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            value={textFieldThreeZeroSevenFiveTwoEightEightOneValue}
+            onChange={(event) => {
+              setTextFieldThreeZeroSevenFiveTwoEightEightOneValue(
+                event.target.value
+              );
+            }}
+            {...getOverrideProps(overrides, "TextField30752881")}
+          ></TextField>
+        </Flex>
+        <Divider
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Divider30752883")}
+        ></Divider>
+        <Button
+          display="flex"
+          gap="0"
+          direction="row"
+          width="fit-content"
+          justifyContent="center"
+          alignItems="center"
+          shrink="0"
+          position="relative"
+          size="default"
+          isDisabled={false}
+          variation="primary"
+          onClick={() => {
+            buttonOnClick();
+          }}
+          {...getOverrideProps(overrides, "Button")}
+        ></Button>
+      </Flex>
+    </Flex>
+  );
+}
